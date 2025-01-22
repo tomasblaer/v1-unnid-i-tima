daa11bc2f76e6dcc2d27bf532559e30e46a42c37
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2025-01-22 17:02:11 +0000

    útfæra skrif á index.html

--- main.js
+++ main.js
@@ -3,22 +3,103 @@ import path from 'node:path';
 
 const INDEX_PATH = './data/index.json';
 
+/**
+ * Les skrá og skilar gögnum eða null.
+ * @param {string} filePath Skráin sem á að lesa
+ * @returns {Promise<unknown | null>} Les skrá úr `filePath` og skilar innihaldi. Skilar `null` ef villa kom upp.
+ */
 async function readJson(filePath) {
   console.log('starting to read', filePath);
+  let data;
   try {
-    const data = await fs.readFile(path.resolve(filePath), 'utf-8');
-    return JSON.parse(data);
+    data = await fs.readFile(path.resolve(filePath), 'utf-8');
   } catch (error) {
     console.error(`Error reading file ${filePath}:`, error.message);
     return null;
   }
+
+  try {
+    const parsed = JSON.parse(data);
+    return parsed;
+  } catch (error) {
+    console.error('error parsing data as json');
+    return null;
+  }
 }
 
+/**
+ * Skrifa HTML fyrir yfirlit í index.html
+ * @param {any} data Gögn til að skrifa
+ * @returns {Promise<void>}
+ */
+async function writeHtml(data) {
+  const htmlFilePath = 'dist/index.html';
+
+  /*
+  <ul>
+    <li>HTML</li>
+    <li>CSS</li>
+  */
+
+  //let html = '';
+
+  /*
+  for (let i = 0; i < data.length; i++) {
+    html += `<li>${data[i].title}</li>\n`;
+  }
+  */
+
+  /*
+  for (let item of data) {
+    html += `<li>${item.title}</li>\n`;
+  }
+  */
+
+  const html = data.map((item) => `<li>${item.title}</li>`).join('\n');
+
+  // EKKI GOTT HTML!
+  const htmlContent = `
+<!doctype html>
+<html>
+  <head>
+    <title>v1</title>
+  </head>
+  <body>
+    <ul>
+      ${html}
+    </ul>
+  </body>
+</html>
+`;
+
+  fs.writeFile(htmlFilePath, htmlContent, 'utf8');
+}
+
+/**
+ *
+ * @param {unknown} data
+ * @returns {any}
+ */
+function parseIndexJson(data) {
+  return data;
+}
+
+/**
+ * Keyrir forritið okkar:
+ * 1. Sækir gögn
+ * 2. Staðfestir gögn (validation)
+ * 3. Skrifar út HTML
+ */
 async function main() {
-  console.log('hello world');
+  const indexJson = await readJson(INDEX_PATH);
 
-  const indexData = await readJson(INDEX_PATH);
+  const indexData = parseIndexJson(indexJson);
 
+  writeHtml(indexData);
+
+  console.log(indexData);
+
+  /*
   if (!Array.isArray(indexData)) {
     console.error('index.json is not an array. Check the file format.');
     return [];
@@ -32,6 +113,7 @@ async function main() {
       return fileData ? { ...item, content: fileData } : null;
     }),
   );
+  */
 }
 
 main();
--- readme.md
+++ readme.md
@@ -1,3 +1,14 @@
 # Vefforritun 2 2025, verkefni 1, vinnsla í tímum
 
 Grunnur/partar af [verkefni 1](https://github.com/vefforritun/vef2-2025-v1) unnir í fyrirlestrartímum.
+
+## Keyrsla
+
+Þarf node útgáfu 22 að minnsta kosti.
+
+Keyrum með:
+
+```bash
+npm install
+node main.js
+```
